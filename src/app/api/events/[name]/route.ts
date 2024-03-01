e8dca16bd1e75ef27d4f123fd6e648beee2b7db6
Rahul M. Navneeth <50259284+RahulNavneeth@users.noreply.github.com>
2024-03-01 07:45:38 +0530

    Update route.ts

--- src/app/api/events/[name]/route.ts
+++ src/app/api/events/[name]/route.ts
@@ -1,6 +1,7 @@
 import { FOLDER_URL, INFO_URL } from "@/libs/utils";
 import { NextRequest, NextResponse } from "next/server";
 
+export const revalidate = 1;
 export async function GET(req: NextRequest, { params }: { params: { name: string } }) {
     const { name } = params;
     const json = await fetch(INFO_URL(name || "")).then(res => res.json());
